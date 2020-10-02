0d87f3bcce3e9f163f393f35437d3fee1b64bf16
Rahul <rahulsai@basina.com>
2020-10-02 17:59:51 +0530

    App

--- WordGuessingGame/App.js
+++ WordGuessingGame/App.js
@@ -1,11 +1,11 @@
 import React,{Component} from 'react';
 import {Text,View} from 'react-native';
-import UserInput from './src/UserInput.js';
+import Maincomponent from './src/Maincomponent.js';
 class App extends Component{
   render(){
     return (
      
-        <UserInput/>
+        <Maincomponent/>
      
     )
   }
