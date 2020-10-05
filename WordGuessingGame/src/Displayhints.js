e62469adabd8b184cc23703ca78796d370328f83
Rahul <rahulsai@basina.com>
2020-10-05 15:39:46 +0530

    displaying hints

---
+++ WordGuessingGame/src/Displayhints.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableHighlight } from 'react-native'
+
+class Display extends Component {
+    state = {
+        singleArray: [],   
+        types: [], 
+        scores: [], 
+        
+        Hints: [],  
+        currentWord: '',
+    }
+}
