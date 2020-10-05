48600be76e1f29c029543547a8ea3de42c216ec1
Rahul <rahulsai@basina.com>
2020-10-05 17:06:27 +0530

    display hints

--- WordGuessingGame/src/Displayhints.js
+++ WordGuessingGame/src/Displayhints.js
@@ -1,13 +1,89 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableHighlight } from 'react-native'
 
-class Display extends Component {
+class Displayhints extends Component {
     state = {
         singleArray: [],   
         types: [], 
         scores: [], 
-        
+
         Hints: [],  
         currentWord: '',
     }
+    render() {
+        if (this.props.indexToShow !== -1) {
+            if (this.props.currentWordHistory === true) {
+                return (
+                    <ScrollView>
+                        {this.props.totalData.map((item, index)=>(
+                            index === 0 ? (
+                                this.FirstHintToDisplay(this.props.typesofHints, index, item)
+                            ) : (this.props.RevealedHints) ? (
+                                this.props.RevealedHints.includes(index) ? (
+                                    this.hintsPresent(this.props.typesofHints, index, item)
+                                ) : (
+                                        this.hintsNotPresent(this.props.typesofHints, index)
+                                    )
+                            ) : (
+                                    this.hintsNotPresent(this.props.typesofHints, index)
+                                )
+                        )
+                        )}
+                    </ScrollView >
+                )
+            }
+            else {
+                return (
+                    <ScrollView>
+                        {this.props.totalData.map((item, index) => (
+                            (this.props.RevealedHints) ? (
+                                this.props.RevealedHints.includes(index) ? (
+                                    this.hintsPresent(this.props.typesofHints, index, item)
+                                ) : (
+                                        this.hintsNotPresentForHistory(this.props.typesofHints, index, item)
+                                    )
+                            ) : (
+                                    this.hintsNotPresentForHistory(this.props.typesofHints, index, item)
+                                )
+                        )
+                        )}
+                    </ScrollView >
+                )
+            }
+        }
+        else {
+            if (this.state.Hints.length > 0) {
+                return (
+                    <ScrollView>
+                        {this.state.singleArray.map((item, index) => (
+                            index === 0 ? (
+                                this.FirstHintToDisplay(this.state.types, index, item)
+                            ) : (this.state.Hints.includes(index)) ? (
+                                this.hintsPresent(this.state.types, index, item)
+                            ) : (
+                                        this.hintsNotPresent(this.state.types, index)
+                                    )
+                        )
+                        )}
+                    </ScrollView>
+                )
+            }
+            else {
+                this.createList()
+                return (
+                    <ScrollView>
+                        {this.state.singleArray.map((item, index) => (
+                            index === 0 ? (
+                                this.FirstHintToDisplay(this.state.types, index, item)
+                            ) : (
+                                    this.hintsNotPresent(this.state.types, index)
+                                )
+                        )
+                        )}
+                    </ScrollView>
+                );
+            }
+        }
+    }
 }
+export default Displayhints;
